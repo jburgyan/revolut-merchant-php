7d4765b0343a6d041bd0eafb9e9cc8c1bec51eea
Jozsef Burgyan <joe2night@gmail.com>
2025-04-16 14:45:09 +0200

    modifiche sul CustomerPaymentMethod e MethodDetails

--- src/Responses/CustomerPaymentMethod.php
+++ src/Responses/CustomerPaymentMethod.php
@@ -6,10 +6,45 @@ namespace Flowwow\RevolutMerchant\Responses;
 
 use Spatie\DataTransferObject\DataTransferObject;
 
+/**
+ * A customer's payment method
+ */
 class CustomerPaymentMethod extends DataTransferObject
 {
-    public int           $id;
-    public string        $type;
-    public string        $saved_for;
-    public MethodDetails $method_details;
+	/**
+	 * The ID of the payment method.
+	 *
+	 * @var string
+	 */
+	public string $id;
+
+	/**
+	 * Possible values: [CARD, REVOLUT_PAY]
+	 *
+	 * The type of the payment method.
+	 * note
+	 *
+	 * Only merchant initiated transactions are supported for saved REVOLUT_PAY payment methods.
+	 *
+	 * @var string|null
+	 */
+	public ?string $type;
+
+	/**
+	 * Possible values: [CUSTOMER, MERCHANT]
+	 *
+	 * Indicates in which case this saved payment method can be used for payments.
+	 *
+	 * CUSTOMER: This payment method can be used only when the customer is on the checkout page.
+	 * MERCHANT: This payment method can be used without the customer being on the checkout page,
+	 * and the merchant can initiate transactions, for example, to take payments for recurring transactions.
+	 *
+	 * @var string|null
+	 */
+	public ?string $saved_for;
+
+	/**
+	 * @var MethodDetails|null
+	 */
+	public ?MethodDetails $method_details;
 }
--- src/Responses/MethodDetails.php
+++ src/Responses/MethodDetails.php
@@ -16,7 +16,7 @@ class MethodDetails extends DataTransferObject
 	 *
 	 * The BIN of the payment card.
 	 *
-	 * @var string|null
+	 * @var string|null $bin
 	 */
 	public ?string $bin;
 
@@ -25,7 +25,7 @@ class MethodDetails extends DataTransferObject
 	 *
 	 * The last four digits of the payment card.
 	 *
-	 * @var string|null
+	 * @var string|null $last4
 	 */
 	public ?string $last4;
 
@@ -39,23 +39,62 @@ class MethodDetails extends DataTransferObject
 	/**
 	 * The expiry year of the payment card.
 	 *
-	 * @var int|null
+	 * @var int|null $expiry_year
 	 */
 	public ?int $expiry_year;
 
 	/**
 	 * The name of the cardholder.
 	 *
-	 * @var string|null
+	 * @var string|null $cardholder_name
 	 */
 	public ?string $cardholder_name;
 
 	/**
 	 * The billing address of the payment method.
 	 *
-	 * @var Address|null
+	 * @var Address|null $billing_address
 	 */
 	public ?Address $billing_address;
+
+	/**
+	 * Possible values: [VISA, MASTERCARD, MAESTRO]
+	 *
+	 * The brand of the payment card.
+	 *
+	 * @var string|null $brand
+	 */
+	public ?string $brand;
+
+	/**
+	 * Possible values: [DEBIT, CREDIT, PREPAID, DEFERRED_DEBIT, CHARGE]
+	 *
+	 * The funding type of the payment card.
+	 *
+	 * @var string|null $funding
+	 */
+	public ?string $funding;
+
+	/**
+	 * The issuer of the payment card.
+	 *
+	 * @var string|null $issuer
+	 */
+	public ?string $issuer;
+
+	/**
+	 * Two-letter country code of the country where the payment card was issued.
+	 *
+	 * @var string|null $issuer_country
+	 */
+	public ?string $issuer_country;
+
+	/**
+	 * The date and time the payment card was added.
+	 *
+	 * @var string|null $created_at
+	 */
+	public ?string $created_at;
 
 
 }
